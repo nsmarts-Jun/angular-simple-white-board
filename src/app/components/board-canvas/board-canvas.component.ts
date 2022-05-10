6aa86bc57f043ec8b93a49f619e4fa2427c01333
park9379 <bjh@nsmarts.co.kr>
2022-05-10 13:33:11 +0900

    코드 라인 정리

--- src/app/components/board-canvas/board-canvas.component.ts
+++ src/app/components/board-canvas/board-canvas.component.ts
@@ -20,240 +20,240 @@ import { EditInfoService } from 'src/@wb/store/edit-info.service';
 
 
 export interface DialogData {
-  title: string;
-  content: string;
+    title: string;
+    content: string;
 }
 
 @Component({
-  selector: 'app-board-canvas',
-  templateUrl: './board-canvas.component.html',
-  styleUrls: ['./board-canvas.component.scss']
+    selector: 'app-board-canvas',
+    templateUrl: './board-canvas.component.html',
+    styleUrls: ['./board-canvas.component.scss']
 })
 export class BoardCanvasComponent implements OnInit, OnDestroy {
 
-  private unsubscribe$ = new Subject<void>();
+    private unsubscribe$ = new Subject<void>();
 
-  editDisabled = true;
-  dragOn = true;
-  currentToolInfo = {
-    type: '',
-    color: '',
-    width: '',
-  };
+    editDisabled = true;
+    dragOn = true;
+    currentToolInfo = {
+        type: '',
+        color: '',
+        width: '',
+    };
 
-  // ************* Subject
-  canvasClearBoardA$;
+    // ************* Subject
+    canvasClearBoardA$;
 
-  // ************* Subject
+    // ************* Subject
 
-  // static: https://stackoverflow.com/questions/56359504/how-should-i-use-the-new-static-option-for-viewchild-in-angular-8
-  @ViewChild('canvasContainer', { static: true }) public canvasContainerRef: ElementRef;
-  @ViewChild('canvasCover', { static: true }) public coverCanvasRef: ElementRef;
-  @ViewChild('teacherCanvas', { static: true }) public teacherCanvasRef: ElementRef;
+    // static: https://stackoverflow.com/questions/56359504/how-should-i-use-the-new-static-option-for-viewchild-in-angular-8
+    @ViewChild('canvasContainer', { static: true }) public canvasContainerRef: ElementRef;
+    @ViewChild('canvasCover', { static: true }) public coverCanvasRef: ElementRef;
+    @ViewChild('teacherCanvas', { static: true }) public teacherCanvasRef: ElementRef;
 
 
-  canvasContainer: HTMLDivElement;
-  coverCanvas: HTMLCanvasElement;
-  teacherCanvas: HTMLCanvasElement;
+    canvasContainer: HTMLDivElement;
+    coverCanvas: HTMLCanvasElement;
+    teacherCanvas: HTMLCanvasElement;
 
-  rendererEvent1: any;
+    rendererEvent1: any;
 
-  constructor(
-    private viewInfoService: ViewInfoService,
-    private editInfoService: EditInfoService,
+    constructor(
+        private viewInfoService: ViewInfoService,
+        private editInfoService: EditInfoService,
 
-    private canvasService: CanvasService,
-    private renderingService: RenderingService,
-    private eventBusService: EventBusService,
-    private renderer: Renderer2,
-    private drawStorageService: DrawStorageService,
-    private drawingService: DrawingService,
+        private canvasService: CanvasService,
+        private renderingService: RenderingService,
+        private eventBusService: EventBusService,
+        private renderer: Renderer2,
+        private drawStorageService: DrawStorageService,
+        private drawingService: DrawingService,
 
-  ) {
+    ) {
 
-  }
-
-  // Resize Event Listener
-  @HostListener('window:resize') resize() {
-    const newWidth = window.innerWidth - CANVAS_CONFIG.sidebarWidth;
-    const newHeight = window.innerHeight - CANVAS_CONFIG.navbarHeight;
-    // sidenav 열릴때 resize event 발생... 방지용도.
-    if (CANVAS_CONFIG.maxContainerWidth === newWidth && CANVAS_CONFIG.maxContainerHeight === newHeight) {
-      return;
     }
-    CANVAS_CONFIG.maxContainerWidth = newWidth;
-    CANVAS_CONFIG.maxContainerHeight = newHeight;
-    this.onResize();
-  }
+
+    // Resize Event Listener
+    @HostListener('window:resize') resize() {
+        const newWidth = window.innerWidth - CANVAS_CONFIG.sidebarWidth;
+        const newHeight = window.innerHeight - CANVAS_CONFIG.navbarHeight;
+        // sidenav 열릴때 resize event 발생... 방지용도.
+        if (CANVAS_CONFIG.maxContainerWidth === newWidth && CANVAS_CONFIG.maxContainerHeight === newHeight) {
+            return;
+        }
+        CANVAS_CONFIG.maxContainerWidth = newWidth;
+        CANVAS_CONFIG.maxContainerHeight = newHeight;
+        this.onResize();
+    }
 
 
-  ngOnInit(): void {
+    ngOnInit(): void {
 
-    // canvas Element 할당
-    this.coverCanvas = this.coverCanvasRef.nativeElement;
-    this.teacherCanvas = this.teacherCanvasRef.nativeElement;
-    this.canvasContainer = this.canvasContainerRef.nativeElement;
-
-
-    // page 변경
-    this.viewInfoService.state$
-      .pipe(takeUntil(this.unsubscribe$), distinctUntilChanged())
-      .subscribe((viewInfo) => {
-        this.onChangePage();
-      });
+        // canvas Element 할당
+        this.coverCanvas = this.coverCanvasRef.nativeElement;
+        this.teacherCanvas = this.teacherCanvasRef.nativeElement;
+        this.canvasContainer = this.canvasContainerRef.nativeElement;
 
 
-
-    // Tool update(nav Menu)에 따른 event handler 변경
-    this.editInfoService.state$
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe((editInfo) => {
-        console.log('[Editor Setting]: ', editInfo);
-
-        this.editDisabled = editInfo.toolDisabled || editInfo.editDisabled;
-
-        // drag Enable
-        this.dragOn = false;
-        if (editInfo.mode == 'move') this.dragOn = true;
-
-        const currentTool = editInfo.tool;
-        this.currentToolInfo = {
-          type: editInfo.tool, // pen, eraser
-          color: editInfo.toolsConfig[currentTool].color,
-          width: editInfo.toolsConfig[currentTool].width
-        };
-        console.log(this.currentToolInfo)
-
-        const zoomScale = this.viewInfoService.state.zoomScale;
-
-        // canvas Event Handler 설정
-        this.canvasService.addEventHandler(this.coverCanvas, this.teacherCanvas, this.currentToolInfo, zoomScale);
-      });
-
-
-    // continer scroll Listener : thumbnail의 window 처리 용도
-    this.rendererEvent1 = this.renderer.listen(this.canvasContainer, 'scroll', event => {
-      this.onScroll();
-    });
-
-    this.eventBusListeners();
-  }
-
-  // end of ngOnInit
-  ngOnDestroy() {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-
-    // render listener 해제
-    this.rendererEvent1();
-  }
-
-  eventBusListeners() {
-    // board-nav로 부터 현재 페이지 드로잉 이벤트 삭제 
-    // 다시 페이지 렌더링
-    this.eventBusService.on('rmoveDrawEventPageRendering',this.unsubscribe$,(data)=>{
-      const viewInfo = this.viewInfoService.state;
-      //document Number -> 1부터 시작.
-      const pageNum = viewInfo.currentPage;
-      const zoomScale = viewInfo.zoomScale;
-      this.pageRender(pageNum, zoomScale)
-    })
-  }
-
-  /**
-   * 판서 Rendering
-   *
-   * @param currentPage
-   * @param zoomScale
-   */
-  async pageRender(currentPage, zoomScale) {
-    console.log('>>> page Board Render!');
-
-    // board rendering
-    const drawingEvents = this.drawStorageService.getDrawingEvents(currentPage);
-    this.renderingService.renderBoard(this.teacherCanvas, zoomScale, drawingEvents);
-  }
+        // page 변경
+        this.viewInfoService.state$
+            .pipe(takeUntil(this.unsubscribe$), distinctUntilChanged())
+            .subscribe((viewInfo) => {
+                this.onChangePage();
+            });
 
 
 
-  /**
-   * Canvas size 설정
-   *
-   * @param currentPage
-   * @param zoomScale
-   * @returns
-   */
-  setCanvasSize(currentPage, zoomScale) {
-    const ratio = this.canvasService.setCanvasSize(currentPage, zoomScale, this.canvasContainer, this.coverCanvas, this.teacherCanvas);
-    return ratio;
-  }
+        // Tool update(nav Menu)에 따른 event handler 변경
+        this.editInfoService.state$
+            .pipe(takeUntil(this.unsubscribe$))
+            .subscribe((editInfo) => {
+                console.log('[Editor Setting]: ', editInfo);
 
-  /**
-   * 창 크기 변경시
-   *
-   */
-  onResize() {
-    // Resize시 container size 조절.
-    const ratio = this.canvasService.setContainerSize(this.coverCanvas, this.canvasContainer);
+                this.editDisabled = editInfo.toolDisabled || editInfo.editDisabled;
 
-    // thumbnail window 크기 변경을 위한 처리.
-    this.eventBusService.emit(new EventData("change:containerSize", {
-      ratio,
-      coverWidth: this.coverCanvas.width,
-    }));
+                // drag Enable
+                this.dragOn = false;
+                if (editInfo.mode == 'move') this.dragOn = true;
 
-  }
+                const currentTool = editInfo.tool;
+                this.currentToolInfo = {
+                    type: editInfo.tool, // pen, eraser
+                    color: editInfo.toolsConfig[currentTool].color,
+                    width: editInfo.toolsConfig[currentTool].width
+                };
+                console.log(this.currentToolInfo)
 
-  /**
-   * Scroll 발생 시
-   */
-  onScroll() {
-    this.eventBusService.emit(new EventData('change:containerScroll', {
-      left: this.canvasContainer.scrollLeft,
-      top: this.canvasContainer.scrollTop
-    }))
-  }
+                const zoomScale = this.viewInfoService.state.zoomScale;
+
+                // canvas Event Handler 설정
+                this.canvasService.addEventHandler(this.coverCanvas, this.teacherCanvas, this.currentToolInfo, zoomScale);
+            });
 
 
-  /**
-     * change Page : 아래 사항에 대해 공통으로 사용
-     * - 최초 Load된 경우
-     * - 페이지 변경하는 경우
-     * - scale 변경하는 경우
+        // continer scroll Listener : thumbnail의 window 처리 용도
+        this.rendererEvent1 = this.renderer.listen(this.canvasContainer, 'scroll', event => {
+            this.onScroll();
+        });
+
+        this.eventBusListeners();
+    }
+
+    // end of ngOnInit
+    ngOnDestroy() {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
+
+        // render listener 해제
+        this.rendererEvent1();
+    }
+
+    eventBusListeners() {
+        // board-nav로 부터 현재 페이지 드로잉 이벤트 삭제 
+        // 다시 페이지 렌더링
+        this.eventBusService.on('rmoveDrawEventPageRendering', this.unsubscribe$, (data) => {
+            const viewInfo = this.viewInfoService.state;
+            //document Number -> 1부터 시작.
+            const pageNum = viewInfo.currentPage;
+            const zoomScale = viewInfo.zoomScale;
+            this.pageRender(pageNum, zoomScale)
+        })
+    }
+
+    /**
+     * 판서 Rendering
+     *
+     * @param currentPage
+     * @param zoomScale
      */
-  onChangePage() {
+    async pageRender(currentPage, zoomScale) {
+        console.log('>>> page Board Render!');
 
-    const viewInfo = this.viewInfoService.state;
-
-    //document Number -> 1부터 시작.
-    const pageNum = viewInfo.currentPage;
-    const zoomScale = viewInfo.zoomScale;
-
-    console.log(`>> changePage to page: ${pageNum}, scale: ${zoomScale} `);
-
-    // 기존의 rx drawing event 삭제: 다른 page에 그려지는 현상 방지
-    // this.drawingService.stopRxDrawing();
-
-    // Canvas Size 설정: ratio는 canvas container와 실제 canvas의 비율
-    const ratio = this.setCanvasSize(pageNum, zoomScale);
-
-    // Thumbnail window 조정
-    this.eventBusService.emit(new EventData('change:containerSize', {
-      ratio,
-      coverWidth: this.coverCanvas.width,
-    }));
-
-    // Board Render
-    this.pageRender(pageNum, zoomScale);
-
-    // Canvas Event Set (zoom 조절)
-    this.canvasService.addEventHandler(this.coverCanvas, this.teacherCanvas, this.currentToolInfo, zoomScale);
+        // board rendering
+        const drawingEvents = this.drawStorageService.getDrawingEvents(currentPage);
+        this.renderingService.renderBoard(this.teacherCanvas, zoomScale, drawingEvents);
+    }
 
 
-    // scroll bar가 있는 경우 page 전환 시 초기 위치로 변경
-    this.canvasContainer.scrollTop = 0;
-    this.canvasContainer.scrollLeft = 0;
-  };
+
+    /**
+     * Canvas size 설정
+     *
+     * @param currentPage
+     * @param zoomScale
+     * @returns
+     */
+    setCanvasSize(currentPage, zoomScale) {
+        const ratio = this.canvasService.setCanvasSize(currentPage, zoomScale, this.canvasContainer, this.coverCanvas, this.teacherCanvas);
+        return ratio;
+    }
+
+    /**
+     * 창 크기 변경시
+     *
+     */
+    onResize() {
+        // Resize시 container size 조절.
+        const ratio = this.canvasService.setContainerSize(this.coverCanvas, this.canvasContainer);
+
+        // thumbnail window 크기 변경을 위한 처리.
+        this.eventBusService.emit(new EventData("change:containerSize", {
+            ratio,
+            coverWidth: this.coverCanvas.width,
+        }));
+
+    }
+
+    /**
+     * Scroll 발생 시
+     */
+    onScroll() {
+        this.eventBusService.emit(new EventData('change:containerScroll', {
+            left: this.canvasContainer.scrollLeft,
+            top: this.canvasContainer.scrollTop
+        }))
+    }
+
+
+    /**
+       * change Page : 아래 사항에 대해 공통으로 사용
+       * - 최초 Load된 경우
+       * - 페이지 변경하는 경우
+       * - scale 변경하는 경우
+       */
+    onChangePage() {
+
+        const viewInfo = this.viewInfoService.state;
+
+        //document Number -> 1부터 시작.
+        const pageNum = viewInfo.currentPage;
+        const zoomScale = viewInfo.zoomScale;
+
+        console.log(`>> changePage to page: ${pageNum}, scale: ${zoomScale} `);
+
+        // 기존의 rx drawing event 삭제: 다른 page에 그려지는 현상 방지
+        // this.drawingService.stopRxDrawing();
+
+        // Canvas Size 설정: ratio는 canvas container와 실제 canvas의 비율
+        const ratio = this.setCanvasSize(pageNum, zoomScale);
+
+        // Thumbnail window 조정
+        this.eventBusService.emit(new EventData('change:containerSize', {
+            ratio,
+            coverWidth: this.coverCanvas.width,
+        }));
+
+        // Board Render
+        this.pageRender(pageNum, zoomScale);
+
+        // Canvas Event Set (zoom 조절)
+        this.canvasService.addEventHandler(this.coverCanvas, this.teacherCanvas, this.currentToolInfo, zoomScale);
+
+
+        // scroll bar가 있는 경우 page 전환 시 초기 위치로 변경
+        this.canvasContainer.scrollTop = 0;
+        this.canvasContainer.scrollLeft = 0;
+    };
 
 
 
--- src/app/components/board-nav/board-nav.component.ts
+++ src/app/components/board-nav/board-nav.component.ts
@@ -18,100 +18,100 @@ import shapeOutlineIcon from '@iconify/icons-mdi/shape-outline';
 
 
 @Component({
-  selector: 'app-board-nav',
-  templateUrl: './board-nav.component.html',
-  styleUrls: ['./board-nav.component.scss']
+    selector: 'app-board-nav',
+    templateUrl: './board-nav.component.html',
+    styleUrls: ['./board-nav.component.scss']
 })
 export class BoardNavComponent implements OnInit {
-  isSyncMode: boolean;
-  colorList = [
-    { color: 'black' },
-    { color: 'white' },
-    { color: 'red' },
-    { color: 'blue' },
-    { color: 'green' },
-    { color: 'yellow' }
-  ]
-  currentColor = 'black';
-  currentTool: string = 'pen';
-  menuName: any;
-  numPages: any;
-  currentPageNum: any;
+    isSyncMode: boolean;
+    colorList = [
+        { color: 'black' },
+        { color: 'white' },
+        { color: 'red' },
+        { color: 'blue' },
+        { color: 'green' },
+        { color: 'yellow' }
+    ]
+    currentColor = 'black';
+    currentTool: string = 'pen';
+    menuName: any;
+    numPages: any;
+    currentPageNum: any;
 
-  // iconify TEST //////////////////////
-  eraserIcon = eraserIcon;
-  shapeOutlineIcon = shapeOutlineIcon;
-  markerIcon = markerIcon;
-  //////////////////////////////////////
+    // iconify TEST //////////////////////
+    eraserIcon = eraserIcon;
+    shapeOutlineIcon = shapeOutlineIcon;
+    markerIcon = markerIcon;
+    //////////////////////////////////////
 
-  // Width: 3단계 설정
-  widthSet = CANVAS_CONFIG.widthSet;
-  currentWidth = {
-    pointer: this.widthSet.pointer[0],
-    pen: this.widthSet.pen[0],
-    highlighter: this.widthSet.highlighter[0],
-    eraser: this.widthSet.eraser[2],
-    line: this.widthSet.line[0],
-    circle: this.widthSet.circle[0],
-    rectangle: this.widthSet.rectangle[0],
-    roundedRectangle: this.widthSet.roundedRectangle[0],
-  };
-  mode: any = 'move';
+    // Width: 3단계 설정
+    widthSet = CANVAS_CONFIG.widthSet;
+    currentWidth = {
+        pointer: this.widthSet.pointer[0],
+        pen: this.widthSet.pen[0],
+        highlighter: this.widthSet.highlighter[0],
+        eraser: this.widthSet.eraser[2],
+        line: this.widthSet.line[0],
+        circle: this.widthSet.circle[0],
+        rectangle: this.widthSet.rectangle[0],
+        roundedRectangle: this.widthSet.roundedRectangle[0],
+    };
+    mode: any = 'move';
 
-  private unsubscribe$ = new Subject<void>();
+    private unsubscribe$ = new Subject<void>();
 
-  // canvas editing
-  // https://dev.to/nyxtom/how-to-render-code-blocks-in-canvas-from-a-markdown-editor-4lif
+    // canvas editing
+    // https://dev.to/nyxtom/how-to-render-code-blocks-in-canvas-from-a-markdown-editor-4lif
 
-  constructor(
-    private editInfoService: EditInfoService,
-    private eventBusService: EventBusService,
-    private drawStorageService: DrawStorageService,
-    private viewInfoService: ViewInfoService,
-  ) { }
-
-
-  ngOnInit(): void {
-    
-    // 현재 Page 변경
-    this.viewInfoService.state$
-      .pipe(takeUntil(this.unsubscribe$), pluck('currentPage'), distinctUntilChanged())
-      .subscribe((currentPage) => {
-        this.currentPageNum = currentPage;
-      });
-
-    this.editInfoService.state$
-      .pipe(takeUntil(this.unsubscribe$), distinctUntilChanged())
-      .subscribe((editInfo) => {
-        // console.log(editInfo);
-        this.mode = editInfo.mode;
-        this.currentTool = editInfo.tool;
-        this.currentColor = editInfo.toolsConfig.pen.color;
-        this.currentWidth = {
-          pointer: editInfo.toolsConfig.pointer.width,
-          pen: editInfo.toolsConfig.pen.width,
-          highlighter: editInfo.toolsConfig.highlighter.width,
-          eraser: editInfo.toolsConfig.eraser.width,
-          line: editInfo.toolsConfig.line.width,
-          circle: editInfo.toolsConfig.circle.width,
-          rectangle: editInfo.toolsConfig.rectangle.width,
-          roundedRectangle: editInfo.toolsConfig.roundedRectangle.width,
-        }
-      });
-  }
+    constructor(
+        private editInfoService: EditInfoService,
+        private eventBusService: EventBusService,
+        private drawStorageService: DrawStorageService,
+        private viewInfoService: ViewInfoService,
+    ) { }
 
 
-  /**
-   * 색상 변경
-   *
-   * - 현재 pen인 경우에만 반응
-   * @param color 색상 : 향후 HEXA로 변경 고려
-   *
-   */
-  changeColor(color) {
-    const editInfo = Object.assign({}, this.editInfoService.state);
+    ngOnInit(): void {
 
-    if (editInfo.mode != 'draw' || (editInfo.tool == 'erasar' || editInfo.tool == 'pointer')) return;
+        // 현재 Page 변경
+        this.viewInfoService.state$
+            .pipe(takeUntil(this.unsubscribe$), pluck('currentPage'), distinctUntilChanged())
+            .subscribe((currentPage) => {
+                this.currentPageNum = currentPage;
+            });
+
+        this.editInfoService.state$
+            .pipe(takeUntil(this.unsubscribe$), distinctUntilChanged())
+            .subscribe((editInfo) => {
+                // console.log(editInfo);
+                this.mode = editInfo.mode;
+                this.currentTool = editInfo.tool;
+                this.currentColor = editInfo.toolsConfig.pen.color;
+                this.currentWidth = {
+                    pointer: editInfo.toolsConfig.pointer.width,
+                    pen: editInfo.toolsConfig.pen.width,
+                    highlighter: editInfo.toolsConfig.highlighter.width,
+                    eraser: editInfo.toolsConfig.eraser.width,
+                    line: editInfo.toolsConfig.line.width,
+                    circle: editInfo.toolsConfig.circle.width,
+                    rectangle: editInfo.toolsConfig.rectangle.width,
+                    roundedRectangle: editInfo.toolsConfig.roundedRectangle.width,
+                }
+            });
+    }
+
+
+    /**
+     * 색상 변경
+     *
+     * - 현재 pen인 경우에만 반응
+     * @param color 색상 : 향후 HEXA로 변경 고려
+     *
+     */
+    changeColor(color) {
+        const editInfo = Object.assign({}, this.editInfoService.state);
+
+        if (editInfo.mode != 'draw' || (editInfo.tool == 'erasar' || editInfo.tool == 'pointer')) return;
         editInfo.toolsConfig.pen.color = color;
         editInfo.toolsConfig.highlighter.color = color;
         editInfo.toolsConfig.line.color = color;
@@ -119,66 +119,66 @@ export class BoardNavComponent implements OnInit {
         editInfo.toolsConfig.rectangle.color = color;
         editInfo.toolsConfig.roundedRectangle.color = color;
         this.editInfoService.setEditInfo(editInfo);
-  }
-
-  /**
-   * Width 변경
-   *
-   * -현재 Pen 또는 eraser인 경우에만 반응
-   *
-   * @param width
-   */
-  changeWidth(width) {
-
-    const editInfo = Object.assign({}, this.editInfoService.state);
-
-    if (editInfo.mode != 'draw') return;
-
-    const tool = editInfo.tool; // tool: 'pen', 'eraser', 'shape'
-    editInfo.toolsConfig[tool].width = width;
-
-    this.editInfoService.setEditInfo(editInfo);
-  }
-
-
-  /**
-   * Pen, Eraser 선택
-   *
-   * @param tool : 'pen', 'eraser'
-   *
-   */
-  changeTool(tool) {
-    console.log(tool)
-    const editInfo = Object.assign({}, this.editInfoService.state);
-    if (editInfo.tool == 'eraser' && editInfo.mode == 'draw' && tool == 'eraser') {
-      if(confirm("Do you want to delete all drawings on the current page?")){
-
-          // 자기자신한테 있는 드로우 이벤트 제거
-          this.drawStorageService.clearDrawingEvents(this.currentPageNum);
-          this.eventBusService.emit(new EventData('rmoveDrawEventPageRendering', ''));
-          this.eventBusService.emit(new EventData('rmoveDrawEventThumRendering', ''));
-      } else {
-          return;
-      }
     }
-    editInfo.mode = 'draw';
-    editInfo.tool = tool;
-    this.editInfoService.setEditInfo(editInfo);
 
-    // 지우개 2번 Click은 여기서 check 하는 것이 좋을 듯?
-  }
+    /**
+     * Width 변경
+     *
+     * -현재 Pen 또는 eraser인 경우에만 반응
+     *
+     * @param width
+     */
+    changeWidth(width) {
+
+        const editInfo = Object.assign({}, this.editInfoService.state);
+
+        if (editInfo.mode != 'draw') return;
+
+        const tool = editInfo.tool; // tool: 'pen', 'eraser', 'shape'
+        editInfo.toolsConfig[tool].width = width;
+
+        this.editInfoService.setEditInfo(editInfo);
+    }
 
 
-  /**
-   * Move 선택
-   *
-   * @param mode : 현재는 'move'만 있음 (향후 sync?)
-   *
-   */
-  changeMode(mode) {
-    const editInfo = Object.assign({}, this.editInfoService.state);
-    editInfo.mode = mode;
-    this.editInfoService.setEditInfo(editInfo);
-  }
+    /**
+     * Pen, Eraser 선택
+     *
+     * @param tool : 'pen', 'eraser'
+     *
+     */
+    changeTool(tool) {
+        console.log(tool)
+        const editInfo = Object.assign({}, this.editInfoService.state);
+        if (editInfo.tool == 'eraser' && editInfo.mode == 'draw' && tool == 'eraser') {
+            if (confirm("Do you want to delete all drawings on the current page?")) {
+
+                // 자기자신한테 있는 드로우 이벤트 제거
+                this.drawStorageService.clearDrawingEvents(this.currentPageNum);
+                this.eventBusService.emit(new EventData('rmoveDrawEventPageRendering', ''));
+                this.eventBusService.emit(new EventData('rmoveDrawEventThumRendering', ''));
+            } else {
+                return;
+            }
+        }
+        editInfo.mode = 'draw';
+        editInfo.tool = tool;
+        this.editInfoService.setEditInfo(editInfo);
+
+        // 지우개 2번 Click은 여기서 check 하는 것이 좋을 듯?
+    }
+
+
+    /**
+     * Move 선택
+     *
+     * @param mode : 현재는 'move'만 있음 (향후 sync?)
+     *
+     */
+    changeMode(mode) {
+        const editInfo = Object.assign({}, this.editInfoService.state);
+        editInfo.mode = mode;
+        this.editInfoService.setEditInfo(editInfo);
+    }
 
 }
